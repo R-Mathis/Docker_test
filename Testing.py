f8f278827a1f7bf5c7b9eff467670b09313d225d
R-Mathis <mathis.reine@servier.com>
2022-11-24 16:39:25 +0100

    retravaillé gestion des ports

--- Testing.py
+++ Testing.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+import uvicorn
+import os
+
+print("Hello world")
+
 
 app=FastAPI()
 @app.get("/")
@@ -8,3 +13,7 @@ def main():
 @app.get("/{medicament}")
 def medicaments(medicament:str):
     return{"le nom du medicament est": medicament}
+
+if __name__=="__main__":
+    app_port = int(os.getenv('APP_PORT', 9000))
+    uvicorn.run(app, host="0.0.0.0", port=app_port)
